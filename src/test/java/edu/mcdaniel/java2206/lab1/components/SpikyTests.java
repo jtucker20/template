d09b3478785278a7adc00ae13c72411daf5d7f26
Collin Wainwright <csw0101@mcdaniel.edu>
2020-02-16 13:36:17 -0500

    Step nine of the Lab, modify tests

--- src/test/java/edu/mcdaniel/java2206/lab1/components/SpikyTests.java
+++ src/test/java/edu/mcdaniel/java2206/lab1/components/SpikyTests.java
@@ -7,25 +7,87 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SpikyTests {
 
-    private Spiky exampleClass;
+    private Spiky spikyClass;
 
     @BeforeEach
     void setUp() {
 
-        exampleClass = new Spiky();
+        spikyClass = new Spiky();
 
     }
 
     @Test
-    void getMessage() {
+    void testSpaceFunctionality(){
 
         // Arrange
-        String expected = "Hello World!";
+        String expected = "    ";
 
         // Act
-        String actual = exampleClass.getMessage();
+        String actual = spikyClass.oneLineForward(3, 0);
 
         // Assert
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSpaceFunctionality2sp1spk(){
+        // Arrange
+        String expected = "  \\/";
+
+        // Act
+        String actual = spikyClass.oneLineForward(2, 1);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSpaceFunctionality1sp2spk(){
+
+        // Arrange
+        String expected = " \\\\//";
+
+        // Act
+        String actual = spikyClass.oneLineForward(1, 2);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSpaceFunctionality0sp3spk(){
+
+        // Arrange
+        String expected = "\\\\\\///";
+
+        // Act
+        String actual = spikyClass.oneLineForward(0, 3);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void oneLineBackwards(){
+    }
+
+    @Test
+    void topSpike(){
+
+        // Arrange
+        String[] expected = new String[3];
+        expected[0] = "  \\/";
+        expected[1] = " \\\\//";
+        expected[2] = "\\\\\\///";
+
+        // Act
+        String[] actual = this.spikyClass.topSpike();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void bottomSpike(){
     }
 }
